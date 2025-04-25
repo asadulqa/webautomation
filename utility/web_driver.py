55b8a9a707eed5307654932d06a55c3491f1923d
Asadul Haque <112169569+asadulqa@users.noreply.github.com>
2025-04-25 22:15:45 +0600

    Update web_driver.py

--- utility/web_driver.py
+++ utility/web_driver.py
@@ -1,11 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 class WebDriverFactory:
     @staticmethod
     def get_driver():
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+
         service = Service(ChromeDriverManager().install())
-        driver = webdriver.Chrome(service=service)
-        driver.maximize_window()
+        driver = webdriver.Chrome(service=service, options=chrome_options)
+        driver.set_window_size(1920, 1080)
         return driver
