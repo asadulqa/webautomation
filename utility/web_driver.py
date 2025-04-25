d5d2d036b1bb5487bed88d1cd19058f2a09be906
asadul7 <asadul999.bd@gmail.com>
2025-04-25 23:12:42 +0600

    yml

--- utility/web_driver.py
+++ utility/web_driver.py
@@ -6,12 +6,11 @@ from webdriver_manager.chrome import ChromeDriverManager
 class WebDriverFactory:
     @staticmethod
     def get_driver():
-        chrome_options = Options()
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--disable-dev-shm-usage")
+        options = Options()
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
 
-        service = Service(ChromeDriverManager().install())
-        driver = webdriver.Chrome(service=service, options=chrome_options)
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
         driver.set_window_size(1920, 1080)
         return driver
